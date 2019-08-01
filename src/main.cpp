c9f6091670f6111496dbb64cbe30a01ec6128b03
shiro <shironecko.mail@gmail.com>
2019-08-01 16:56:14 +0300

    remove leftower cog code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,4 @@
 #include <stdio.h>
 
 int main() {
-/*[[[cog
-import cog
-from code_generation import Struct, generateStructDeclaration
-struct = Struct("TestStruct", "")
-generateStructDeclaration(struct, lambda str : cog.outl(str))
-
-# fnames = ['Hello', 'COG', 'C++']
-# for fn in fnames:
-#     cog.outl("printf(\"%s \");" % fn)
-# cog.outl("printf(\"\\n\");")
-]]]*/
-struct TestStruct {
-};
-//[[[end]]]
-
-    printf("Hello, world!\n");
 }
