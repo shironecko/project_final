61dd1d8969bc5dab589f63e6357fec68a6fb4c38
vi <shironecko.mail@gmail.com>
2020-01-15 16:09:40 +0200

    tidy up sharpmake projects a bit

--- src/SharpmakeProjects/SharpmakeMain.cs
+++ src/SharpmakeProjects/SharpmakeMain.cs
@@ -3,33 +3,10 @@ using System.IO;
 using Sharpmake;
 using SharpmakeProjects.Projects;
 
-[module: Include("Options.cs")]
-[module: Include("Projects/GameProject.cs")]
+[module: Include("GameSolution.cs")]
 
 namespace SharpmakeProjects {
-    static class Utils {
-        public static bool IsPosixPlatform() {
-            int p = (int)Environment.OSVersion.Platform;
-            return (p == 4) || (p == 6) || (p == 128);
-        }
-    }
-    
-    [Generate]
-    class GameSolution : Solution {
-        public GameSolution() {
-            Name = "Game";
-
-            var devEnv = Util.IsRunningOnUnix() ? DevEnv.make : DevEnv.vs2019;
-            AddTargets(Options.GetCommonTarget());
-        }
-
-        [Configure]
-        public void ConfigureAll(Solution.Configuration conf, Target target) {
-            conf.SolutionPath = Options.ProjectsRoot;
-            conf.SolutionFileName = "[solution.Name].[target.Platform].[target.DevEnv]";
-            conf.AddProject<GameProject>(target);
-        }
-
+    public static class SharpmakeConfiguration {
         [Main]
         public static void SharpmakeMain(Arguments sharpmakeArgs) { sharpmakeArgs.Generate<GameSolution>(); }
     }
